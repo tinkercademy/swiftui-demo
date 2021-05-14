e4f8810b70c940b95d81dcb937bfd93efab589c7
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-05-14 20:39:42 +0800

    Added animations to advanced clicker 2, added gitignore

--- Advanced Clicker Version 2 - with animations/Advanced Clicker/ClickerContentView.swift
+++ Advanced Clicker Version 2 - with animations/Advanced Clicker/ClickerContentView.swift
@@ -26,29 +26,68 @@ struct ClickerContentView: View {
     //   This timer will fire every 0.1 seconds. There is
     //   a snippet at the bottom where it handles what
     //   happens when the timer is fired.
+    // This timer is not fired yet
     var timer = Timer.publish(every: 0.1,
                               on: .main,
                               in: .common).autoconnect()
     
+    @State private var scale: CGFloat = 0
+    
+    @State private var countdown: Int = 3
+    
+    
+    var colors: [Color] = [.yellow, .orange, .red, .pink]
     var body: some View {
         
-        VStack {
+        ZStack {
+            Text("3")
+                .bold()
+                .font(.system(size: 100, weight: .bold, design: .default))
+                .scaleEffect(countdown == 3 ? scale : 0)
+                .opacity(countdown == 3 ? Double(scale) : 1)
+                .animation(.easeInOut)
+                
+            Text("2")
+                .bold()
+                .font(.system(size: 100, weight: .bold, design: .default))
+                .scaleEffect(countdown == 2 ? scale : 0)
+                .opacity(countdown == 2 ? Double(scale) : 1)
+                .animation(.easeInOut)
+                
+            Text("1")
+                .bold()
+                .font(.system(size: 100, weight: .bold, design: .default))
+                .scaleEffect(countdown == 1 ? scale : 0)
+                .opacity(countdown == 1 ? Double(scale) : 1)
+                .animation(.easeInOut)
+            
             Text("\(counter)")
+                .opacity(countdown == 0 ? 1 : 0)
+                .animation(.easeIn)
         }
         // Max width and height so it takes up full screen
         //   Mainly for the tap gesture to work full screen
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
         
         // Change background color!
-        .background(Color.green)
+        .background(colors[countdown])
+        
+        // Enable animations between the background colors
+        .animation(.easeInOut)
         
         // Handling when the user taps the view
         .onTapGesture {
+            // Make sure countdown == 0 before we proceed
+            guard countdown == 0 else { return }
+            
             // Increment counter every tap
             counter += 1
             
             // User has hit 30 clicks!
             if counter == 30 {
+                
+                // Remove the first 3 seconds which was used for countdown
+                time -= 3
                 
                 // Output results onto console
                 print("You done")
@@ -70,6 +109,15 @@ struct ClickerContentView: View {
         //   adds 0.1 to the time variable
         .onReceive(timer) { _ in
             time += 0.1
+            
+            if countdown > 0 {
+                scale += 0.1
+                
+                if scale > 1 {
+                    countdown -= 1
+                    scale = 0
+                }
+            }
         }
     }
 }
